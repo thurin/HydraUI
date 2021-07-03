d6209bac8fd6cdb1765e09f2bec778a30f362db1
Hydra <ryanandrews135@gmail.com>
2021-07-03 01:58:20 -0600

    Ship it

---
+++ HydraUI/Elements/Update.lua
@@ -0,0 +1,151 @@
+local HydraUI, GUI, Language, Assets, Settings = select(2, ...):get()
+
+local tonumber = tonumber
+local SendAddonMessage = C_ChatInfo.SendAddonMessage
+local IsInGuild = IsInGuild
+local IsInGroup = IsInGroup
+local IsInRaid = IsInRaid
+
+local AddOnVersion = tonumber(HydraUI.UIVersion)
+local User = HydraUI.UserName .. "-" .. HydraUI.UserRealm
+
+local Update = HydraUI:NewModule("Update")
+
+
+--[[local WhatsNew = {
+	[1.01] = {
+		"Alert frames",
+		"Version check module",
+	},
+}
+]]
+
+-- display a simple "What's new" list.
+local WhatsNewOnMouseUp = function()
+
+end
+
+-- To be implemented. Add something here like a link or whatever to update.
+local UpdateOnMouseUp = function()
+	HydraUI:print(Language["You can get an updated version of HydraUI at https://www.curseforge.com/wow/addons/hydraui"])
+	print(Language["Join the Discord community for support and feedback https://discord.gg/XGYDaBF"])
+end
+
+function Update:PLAYER_ENTERING_WORLD()
+	--[[if self.NewVersion then
+		HydraUI:SendAlert("What's new?", "Click here to learn more", nil, WhatsNewOnMouseUp, true)
+		
+		self.NewVersion = false
+	end]]
+	
+	if IsInGuild() then
+		SendAddonMessage("HydraUI-Version", AddOnVersion, "GUILD")
+	end
+	
+	if IsInRaid() then
+		if IsInRaid(LE_PARTY_CATEGORY_INSTANCE) then
+			SendAddonMessage("HydraUI-Version", AddOnVersion, "INSTANCE")
+		else
+			SendAddonMessage("HydraUI-Version", AddOnVersion, "RAID")
+		end
+	end
+	
+	if IsInGroup() then
+		if IsInGroup(LE_PARTY_CATEGORY_INSTANCE) then
+			SendAddonMessage("HydraUI-Version", AddOnVersion, "INSTANCE")
+		else
+			SendAddonMessage("HydraUI-Version", AddOnVersion, "PARTY")
+		end
+	end
+end
+
+function Update:GUILD_ROSTER_UPDATE()
+	if IsInGuild() then
+		SendAddonMessage("HydraUI-Version", AddOnVersion, "GUILD")
+	end
+end
+
+function Update:GROUP_ROSTER_UPDATE()
+	if IsInRaid() then
+		if IsInRaid(LE_PARTY_CATEGORY_INSTANCE) then
+			SendAddonMessage("HydraUI-Version", AddOnVersion, "INSTANCE")
+		else
+			SendAddonMessage("HydraUI-Version", AddOnVersion, "RAID")
+		end
+	end
+	
+	if IsInGroup() then
+		if IsInGroup(LE_PARTY_CATEGORY_INSTANCE) then
+			SendAddonMessage("HydraUI-Version", AddOnVersion, "INSTANCE")
+		else
+			SendAddonMessage("HydraUI-Version", AddOnVersion, "PARTY")
+		end
+	end
+end
+
+function Update:VARIABLES_LOADED(event)
+	HydraUI:BindSavedVariable("HydraUIData", "Data")
+	
+	if (not HydraUI.Data.Version) then
+		HydraUI.Data.Version = AddOnVersion
+	end
+	
+	local StoredVersion = HydraUI.Data.Version
+	
+	--[[ You installed a newer version! Yay you. Yes, you.
+	if (AddOnVersion > StoredVersion) then
+		if (WhatsNew[AddOnVersion] and Settings["ui-display-whats-new"]) then
+			self.NewVersion = true -- Let PEW take over from here.
+		end
+	end]]
+	
+	-- Store a new version if needed.
+	if (StoredVersion ~= AddOnVersion) then
+		HydraUI.Data.Version = AddOnVersion
+	end
+	
+	self:UnregisterEvent(event)
+end
+
+function Update:CHAT_MSG_ADDON(event, prefix, message, channel, sender)
+	if (sender == HydraUI.UserName or prefix ~= "HydraUI-Version") then
+		return
+	end
+	
+	message = tonumber(message)
+	
+	if (channel == "WHISPER") then
+		if (message > AddOnVersion) then
+			HydraUI:SendAlert(Language["New Version!"], format(Language["Update to version |cFF%s%s|r"], Settings["ui-header-font-color"], message), nil, UpdateOnMouseUp, true)
+			
+			-- Store this higher version and tell anyone else who asks
+			AddOnVersion = message
+		end
+	else
+		if (AddOnVersion > message) then -- We have a higher version, share it
+			SendAddonMessage("HydraUI-Version", AddOnVersion, "WHISPER", sender)
+		elseif (message > AddOnVersion) then -- We're behind!
+			HydraUI:SendAlert(Language["New Version!"], format(Language["Update to version |cFF%s%s|r"], Settings["ui-header-font-color"], message), nil, UpdateOnMouseUp, true)
+			
+			-- Store this higher version and tell anyone else who asks
+			AddOnVersion = message
+			
+			self:PLAYER_ENTERING_WORLD() -- Tell others that we found a new version
+		end
+	end
+end
+
+function Update:OnEvent(event, ...)
+	if self[event] then
+		self[event](self, event, ...)
+	end
+end
+
+Update:RegisterEvent("VARIABLES_LOADED")
+Update:RegisterEvent("PLAYER_ENTERING_WORLD")
+Update:RegisterEvent("GUILD_ROSTER_UPDATE")
+Update:RegisterEvent("GROUP_ROSTER_UPDATE")
+Update:RegisterEvent("CHAT_MSG_ADDON")
+Update:SetScript("OnEvent", Update.OnEvent)
+
+C_ChatInfo.RegisterAddonMessagePrefix("HydraUI-Version")
